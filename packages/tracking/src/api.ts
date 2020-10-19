b4ea6f1e51a046748626b001f84920cdfc40bd98
Judah Anthony <judah@codecademy.com>
2020-10-19 18:27:02 -0400

    Fix infinite loop

--- packages/tracking/src/api.ts
+++ packages/tracking/src/api.ts
@@ -8,6 +8,12 @@ type BasicStringable =
       toString(): string;
     };
 
+type ApiFetchOptions = {
+  method?: 'GET' | 'POST' | 'PUT' | 'DELETE';
+  query?: Record<string, BasicStringable>;
+  body?: string;
+};
+
 export class API {
   apiBaseUrl: string;
   user?: User;
@@ -27,41 +33,30 @@ export class API {
     endpoint: string,
     query: Record<string, BasicStringable> = {}
   ): Promise<T> {
-    const headers = this.defaultHeaders;
-    const uri = new URL(endpoint, this.apiBaseUrl);
-
     const user = await this.getCurrentUser();
-    const searchParams = new URLSearchParams(uri.search);
-    searchParams.set('authentication_token', user.auth_token);
-    for (const [k, v] of Object.entries(query)) {
-      searchParams.set(k, v.toString());
-    }
-    uri.search = searchParams.toString();
+    query = {
+      ...query,
+      authentication_token: user.auth_token,
+    };
 
-    const response = await fetch(uri.toString(), {
+    const response = await this.fetch(endpoint, {
       method: 'GET',
-      headers,
-      credentials: 'include',
+      query,
     });
 
     return response.json();
   }
 
   async post<T>(endpoint: string, data = {}): Promise<T> {
-    const headers = this.defaultHeaders;
-    const uri = new URL(endpoint, this.apiBaseUrl);
-
     const user = await this.getCurrentUser();
     data = {
       ...data,
       authentication_token: user.auth_token,
     };
 
-    const response = await fetch(uri.toString(), {
+    const response = await this.fetch(endpoint, {
       method: 'POST',
-      headers,
       body: JSON.stringify(data),
-      credentials: 'include',
     });
 
     return response.json();
@@ -83,6 +78,29 @@ export class API {
     navigator.sendBeacon(uri.toString(), form);
   }
 
+  async fetch(
+    endpoint: string,
+    { method = 'GET', query = {}, body = undefined }: ApiFetchOptions
+  ): Promise<Response> {
+    const headers = this.defaultHeaders;
+    const uri = new URL(endpoint, this.apiBaseUrl);
+
+    if (query) {
+      const searchParams = new URLSearchParams(uri.search);
+      for (const [k, v] of Object.entries(query)) {
+        searchParams.set(k, v.toString());
+      }
+      uri.search = searchParams.toString();
+    }
+
+    return fetch(uri.toString(), {
+      method,
+      headers,
+      body,
+      credentials: 'include',
+    });
+  }
+
   async getCurrentUser(): Promise<User> {
     if (this.user) {
       return this.user;
@@ -90,7 +108,7 @@ export class API {
 
     // Prevent multiple concurrent requests to the user endpoint
     if (!this.userRequest) {
-      this.userRequest = this.get<User>('users/web');
+      this.userRequest = this.fetch('users/web', {}).then((r) => r.json());
     }
 
     this.user = await this.userRequest;
